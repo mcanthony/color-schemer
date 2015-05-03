273befae25c38f8d0cc42a79a8318c2b12f61f78
Chris Sattinger <crucialfelix@gmail.com>
2015-05-03 14:21:35 +0200

    basic functionality

--- cli.js
+++ cli.js
@@ -3,30 +3,53 @@
 
 var pkg = require('./package.json');
 var colorSchemer = require('./');
-var argv = process.argv.slice(2);
+var program = require('commander');
 
-function help() {
-  console.log([
-    '',
-      '  ' + pkg.description,
-    '',
-    '  Example',
-    '    color-schemer ',
-    ''
-  ].join('\n'));
-}
-
-if (argv.indexOf('--help') !== -1) {
-  help();
-  return;
-}
-
-if (argv.indexOf('--version') !== -1) {
-  console.log(pkg.version);
-  return;
-}
+program.version(pkg.version);
+program.usage('<command> [options]');
 
 
-colorSchemer(argv[0], function(){
+/**
+ * paletton.xml -> palette.json
+ * color-schemer convert paletton.xml > palette.json
+ */
+program
+  .command('convert <inputFile>')
+  .description('Convert a color scheme file from paletton.xml to a simple json format')
+  .option('-f, --format [value]', 'Input format (default: paletton.com XML)')
+  .action(function(file, options) {
+    var out = colorSchemer.convert(file, options.format);
+    console.log(out);
+  });
 
-});
+
+/**
+ * palette.json -> combos.json
+ * color-schemer combos --threshold=3 palette.json > combos.json
+ */
+program
+  .command('combos <jsonPalette>')
+  .description('Given a color palette JSON file, generate readable color combinations using WCAG readability guidelines')
+  .option('-t, --threshold [value]', 'WCAG readability threshold')
+  .option('-g, --grays [value]', 'Add grays to palette')
+  .action(function(palettePath, options) {
+    var out = colorSchemer.makeCombos(palettePath, options.threshold, options.grays);
+    console.log(out);
+  });
+
+
+/**
+ * map.yaml (combos) -> scheme.json
+ * map.yaml (combos) -> scheme.scss
+ * color-schemer map scheme.yaml combos.json --format=json > scheme.json
+ */
+program
+  .command('map <yamlMap> <jsonPalette>')
+  .description('Export a color scheme from a yamlMap, mapping named values to colors using a JSON palette file')
+  .option('-f, --format [value]', 'Output format')
+  .action(function(yamlMap, jsonPalette, options) {
+    var out = colorSchemer.mapScheme(yamlMap, jsonPalette, options.format);
+    console.log(out);
+  });
+
+program.parse(process.argv);
--- index.js
+++ index.js
@@ -1,7 +1,160 @@
 'use strict';
 
+/**
+ * load colors from a paletton.com xml file
+ * run colors + grey scale through colorable
+ * and filter for readable combos
+ * and write to {palette-hash}.yaml
+ */
+
+var fs = require('fs'),
+   xml = require('libxmljs'),
+   yaml = require('js-yaml'),
+   _ = require('lodash'),
+   colorable = require('colorable');
+
+var defaultColor = '#999999',
+  defaultBg = '#FFFFFF',
+  defaultFg = '#000000';
 
 
-module.exports = function() {
+function loadPaletton(path) {
+  return parsePaletton(fs.readFileSync(path, 'utf8'));
+}
 
+
+/**
+ * convert an XML color scheme from paletton.com
+ * to a simple dict
+ */
+function parsePaletton(doc) {
+  var xmlDoc = xml.parseXml(doc);
+  var palette = {};
+  var colors = xmlDoc.find('//color');
+  _.each(colors, function(c) {
+    palette[c.attr('id').value()] = {
+      rgb: c.attr('rgb').value(),
+      r: c.attr('r0').value(),
+      g: c.attr('g0').value(),
+      b: c.attr('b0').value()
+    };
+  });
+
+  return palette;
+}
+
+
+function addGrays(palette) {
+  var grays =  {
+    'white': '#ffffff',
+    'black': '#000000',
+    'lighten-5': '#fafafa',
+    'lighten-4': '#f5f5f5',
+    'lighten-3': '#eeeeee',
+    'lighten-2': '#e0e0e0',
+    'lighten-1': '#bdbdbd',
+    'gray':      '#9e9e9e',
+    'darken-1': '#757575',
+    'darken-2': '#616161',
+    'darken-3': '#424242',
+    'darken-4': '#212121'
+  };
+  return _.assign(palette, _.mapValues(grays, function(g) { return {rgb: g}; }));
+}
+
+
+/**
+ * expand a palette into a larger list of color combinations
+ * filtered by accessibility
+ *
+ * aa - greater than 4.5 (for normal sized text)
+ * aaLarge - greater than 3 (for bold text or text larger than 24px)
+ * aaa - greater than 7
+ * aaaLarge - greater than 4.5
+ */
+function addCombos(palette, threshold) {
+  var colors = _.mapValues(palette, function(p) {
+    return p.rgb;
+  });
+  var combos = colorable(colors, {threshold: threshold || 0});
+  // sort by hsl
+  combos.sort(function(a, b) {
+    return a.values.hsl > b.values.hsl;
+  });
+  var result = _.assign({}, palette);
+  // console.log(JSON.stringify(palette, null, 2));
+  _.each(combos, function(c) {
+    result[c.name].combinations = _.map(c.combinations, function(fg) {
+      var key = _.findKey(palette, function(p) {
+        return p.rgb === fg.hex;
+      });
+      return key || fg.hex;
+    });
+  });
+
+  // console.log(JSON.stringify(result, null, 2));
+  return result;
+}
+
+
+function mapToScheme(map, palette) {
+  return _.transform(map, function(result, value, key) {
+    if(_.isString(value) && (value.substr(0, 1) === '$')) {
+      result[key] = palette[value.substr(1)] || map['default'] || defaultColor;
+      return true;
+    }
+    if(_.isNumber(value)) {
+      result[key] = value;
+      return true;
+    }
+    // undefined or empty string
+    if(_.isEmpty(value)) {
+      if(key.substr(-3) === '-bg') {
+        value = map['default-bg'] || defaultBg;
+      } else if (key.substr(-3) === '-fg') {
+        value = map['default-fg'] || defaultFg;
+      } else {
+        value = map['default'] || defaultColor;
+      }
+    }
+    result[key] = value;
+  });
+}
+
+
+
+/* ------------------------- commands ------------------------------------- */
+
+function convert(inPath, format) {
+  // console.log(inPath, format);
+  var palette = loadPaletton(inPath);
+  return JSON.stringify(palette, null, 2);
+}
+
+function makeCombos(palettePath, threshold, grays) {
+  var palette = JSON.parse(fs.readFileSync(palettePath, 'utf8'));
+  if(grays) {
+    palette = addGrays(palette);
+  }
+  var expanded = addCombos(palette, threshold || 3);
+  return JSON.stringify(expanded, null, 2);
+}
+
+function mapScheme(yamlMap, jsonPalette, format) {
+  var map = yaml.safeLoad(fs.readFileSync(yamlMap, 'utf8'));
+  var scheme = mapToScheme(map, jsonPalette);
+  return JSON.stringify(scheme, null, 2);
+}
+
+
+module.exports = {
+  // command line functions
+  convert: convert,
+  makeCombos: makeCombos,
+  mapScheme: mapScheme,
+
+  // library functions
+  parsePaletton: parsePaletton,
+  addCombos: addCombos,
+  mapToScheme:mapToScheme
 };
